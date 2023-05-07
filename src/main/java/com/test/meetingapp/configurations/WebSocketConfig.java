aa4dd03c2a4de0f23471ce2fe9f58bb56f81c3a6
CoolTrol <bohdan1234yunior@gmail.com>
2023-05-07 14:57:19 +0300

    Starting websocket config

---
+++ src/main/java/com/test/meetingapp/configurations/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.test.meetingapp.configurations;
+
+import com.test.meetingapp.handlers.SignalingHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final SignalingHandler signalingHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(signalingHandler);
+    }
+}
