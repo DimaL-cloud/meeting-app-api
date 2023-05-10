dcdc72c58b1fbeb30c18ee99cb29ace0bfc5a8c1
Dima Lutsyuk <Dimaconsi@gmail.com>
2023-05-10 13:38:34 +0300

    Changed field and column session_id to uuid

--- src/main/java/com/test/meetingapp/models/Session.java
+++ src/main/java/com/test/meetingapp/models/Session.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
@@ -16,6 +20,7 @@ public class Session {
     @Column(name = "id", nullable = false, unique = true)
     private Integer id;
 
-    @Column(name = "session_id", nullable = false, unique = true)
-    private String sessionId;
+    @Column(name = "uuid", nullable = false, unique = true, columnDefinition = "uuid")
+    @Generated(value = GenerationTime.INSERT)
+    private UUID uuid;
 }
